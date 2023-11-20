75dc3a38d9a26bc492950478a41a8f224885035a
Hipjea <pierre.duverneix@gmail.com>
2023-11-20 17:13:49 +0100

    message_box_class

--- app/helpers/messaging/message_helper.rb
+++ app/helpers/messaging/message_helper.rb
@@ -3,5 +3,13 @@ module Messaging
     def self.user_name(firstname, lastname)
       "#{firstname} #{lastname}"
     end
+
+    def self.message_box_class(message, current_user)
+      return "self" if message.emitter_id != message.user_id && current_user.is_manager?
+
+      return "self" if message.emitter_id == message.user_id && !current_user.is_manager?
+      
+      "emitter"
+    end
   end
 end
