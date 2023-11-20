2f378a4b7da485c80764c7fbecd10c6ec94be031
Hipjea <pierre.duverneix@gmail.com>
2023-11-20 16:23:50 +0100

    engine assets initializer

--- lib/messaging/engine.rb
+++ lib/messaging/engine.rb
@@ -1,5 +1,9 @@
 module Messaging
   class Engine < ::Rails::Engine
     isolate_namespace Messaging
+
+    initializer 'messaging.assets.precompile' do |app|
+      app.config.assets.paths << root.join('app', 'builds').to_s
+    end
   end
 end
