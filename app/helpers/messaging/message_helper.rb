9fe3641ce4c601b80f4cf327a3070c2ed7927856
Hipjea <pierre.duverneix@gmail.com>
2023-12-19 09:27:18 +0100

    syntax

--- app/helpers/messaging/message_helper.rb
+++ app/helpers/messaging/message_helper.rb
@@ -12,6 +12,7 @@ module Messaging
       if current_user.manager? || current_user.admin?
         return "emitter__box user" if message.emitter.student?
       end
+
       return "emitter__box institution" if message.emitter != current_user
 
       "self__box"
